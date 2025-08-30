6bbb3cdca05e9395ba42eb668492530a738313fb
Jawad-coder10 <djawadisaindou@gmail.com>
2025-08-31 00:41:32 +0100

    feat:tache de navigation

---
+++ src/app/about/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function AboutPage() {
+  return (
+    <div>
+      <h1 className="text-4xl font-bold text-center mt-10">À Propos</h1>
+    </div>
+  )
+}
+
+
